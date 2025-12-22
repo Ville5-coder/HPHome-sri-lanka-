817393b5fae512f823b35cab32169607f31fac41
Ville Sandgren <sandgren.ville@gmail.com>
2025-12-22 13:35:24 +0100

    Initial Commit

---
+++ HPHome/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HPHome
+//
+//  Created by Ville Sandgren on 2025-12-22.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
