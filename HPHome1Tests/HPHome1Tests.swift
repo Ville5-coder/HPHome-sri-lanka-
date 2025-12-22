9f63a89dcfe9837bfa05c6d0a67183202009f22d
Ville Sandgren <sandgren.ville@gmail.com>
2025-12-22 16:22:10 +0100

    Initial Commit

---
+++ HPHome1Tests/HPHome1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  HPHome1Tests.swift
+//  HPHome1Tests
+//
+//  Created by Ville Sandgren on 2025-12-22.
+//
+
+import Testing
+
+struct HPHome1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
